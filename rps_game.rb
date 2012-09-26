e34dda253a4cf17e91bdd5b591b80f5cff9adca9
KentoYamamoto <b1010126@fun.ac.jp>
2012-09-26 21:33:26 +0900

    add win_hantei method

--- rps_game.rb
+++ rps_game.rb
@@ -1,14 +1,30 @@
 def rps_game(ary)
-	(0..(ary[0].length - 1)).each do |num| #何回目の手か
+	max = 0
+	ary.each do |pAry|
+		if max <= pAry.length
+			max = pAry.length
+		end
+	end
+	(0..(max - 1)).each do |num| #何回目の手か
 	#	p num
 		handAry = []
 		nanninme = 0
 		ary.each do |personAry| #それぞれの人の手を
-			handAry[nanninme, 0] = [[personAry[num], nanninme]]
+			handAry[nanninme, 0] = [personAry[num]]
 			nanninme = nanninme + 1
 		end
 		p handAry
+		rockFlag = handAry.find { |ele| ele == :rock }
+		scissorsFlag = handAry.find { |ele| ele == :scissors }
+		paperFlag = handAry.find { |ele| ele == :paper }
+		if !(rockFlag || scissorsFlag || paperFlag) #ぜんぶにる
+			return [-1, -1]
+		end
+#		elsif !(rockFlag && scissorsFlag && paperFlag)
+
+
 	end
 end
 
-rps_game([[:rock,:paper,:rock],[:rock,:rock,:scissors]])
+rps_game([[:rock,:paper,:rock, :paper],[:rock,:rock,:scissors],[:paper, :paper, :paper]])
+#b = [1,1,1].find do |a| a == 2 end
