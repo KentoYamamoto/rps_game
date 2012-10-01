e6e7d09ddba0f0a433d416b3f6c56fa7cc4766f1
KentoYamamoto <b1010126@fun.ac.jp>
2012-10-01 21:52:04 +0900

    update easy to read

--- rps_game.rb
+++ rps_game.rb
@@ -1,26 +1,32 @@
 def rps_game(ary)
-	max, handAry = 0, []
-	ary.each do |personAry| max = personAry.length if max <= personAry.length end
-	(0..(max - 1)).each do |turn| #何ターン目か
-		(0..(ary.length - 1)).each do |person| handAry[person, 0] = [(ary[person])[turn]] end
-		result, finish, winner, nanninme, handAry = win_hantei(handAry), 0, 0, 0, []
-		(0..(ary.length - 1)).each do |person|
-			ary[person, 1] = [[nil]]          if (ary[person])[turn] == result
-			finish, winner = finish+1, person if !((ary[person])[turn] == result) && (ary[person])[turn]
-			return [turn, winner]             if finish == 1 && (person >= (ary.length - 1))
-		end
-	end
-	return [-1, -1]
+  turn_max = 0
+  ary.each do |person_ary| turn_max = person_ary.length if turn_max <= person_ary.length end
+  (turn_max - 1).times do |turn| #何ターン目か
+    hand_ary = []
+    number_of_winner = 0
+    winner = 0
+    (ary.length - 1).times do |person| hand_ary[person, 0] = [ary[person][turn]] end
+    result = lose_hantei(hand_ary)
+    (ary.length - 1).times do |person|
+      ary[person, 1] = [[nil]] if (ary[person])[turn] == result
+      if (ary[person][turn] != result) && ary[person][turn]
+        number_of_winner += 1
+        winner = person 
+      end
+    end
+    return [turn, winner] if number_of_winner == 1
+  end
+  return [-1, -1]
 end
 
-def win_hantei(handAry)
-	hantei = 0
-	hantei += 1 if handAry.find{ |ele| ele == :rock}
-	hantei += 2 if handAry.find{ |ele| ele == :scissors}
-	hantei += 4 if handAry.find{ |ele| ele == :paper}
-	if hantei == 1 || hantei == 2 || hantei == 4 || hantei == 7 then return :draw #引き分け
-	elsif hantei == 3 then return :scissors #グーが勝ち
-	elsif hantei == 6 then return :paper #チョキが勝ち
-	elsif hantei == 5 then return :rock #パーが勝ち
-	else                   return nil end #配列がおかしいときはnilを返す（全員の手がnilとか）
+def lose_hantei(hand_ary)
+  hantei = 0
+  hantei += 1 if hand_ary.find{ |ele| ele == :rock}
+  hantei += 2 if hand_ary.find{ |ele| ele == :scissors}
+  hantei += 4 if hand_ary.find{ |ele| ele == :paper}
+  if hantei == 1 || hantei == 2 || hantei == 4 || hantei == 7 then return :draw #引き分け
+  elsif hantei == 3 then return :scissors #グーが勝ち
+  elsif hantei == 6 then return :paper #チョキが勝ち
+  elsif hantei == 5 then return :rock #パーが勝ち
+  else return nil end #配列がおかしいときはnilを返す（全員の手がnilとか）
 end
